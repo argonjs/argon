81c5f16bbaaee1698b532bee69beabe989618901
DNoved1 <dnoved1@aim.com>
2016-03-01 02:38:19 -0500

    Temp commit
    Need to go over recent changes and update as well as finish some
    sections.

--- src/vuforia.ts
+++ src/vuforia.ts
@@ -5,27 +5,47 @@ import {Event, CommandQueue, resolveURL} from './utils.ts'
 import {Reality, RealityService} from './reality.ts'
 import {defined} from './cesium/cesium-imports.ts'
 
+/** An error that occured in vuforia while initializing. */
 export class VuforiaInitError extends Error { constructor(message: string, public code: VuforiaInitErrorCode) { super(message) } }
+/** An error that occured in vuforia while loading a data set. */
 export class VuforiaLoadDataSetError extends Error { constructor(message: string) { super(message) } }
+/** An error that occured in vuforia while unloading a data set. */
 export class VuforiaUnloadDataSetError extends Error { constructor(message: string) { super(message) } }
+/** An error that occured in vuforia while activating a data set. */
 export class VuforiaActivateDataSetError extends Error { constructor(message: string) { super(message) } }
+/** An error that occured in vuforia while deactivating a data set. */
 export class VuforiaDeactivateDataSetError extends Error { constructor(message: string) { super(message) } }
 
-/// Return codes for init() function
+/**
+ * The set of possible error codes that can be returned from vuforia's
+ * initialization function.
+ */
 export enum VuforiaInitErrorCode {
-    INIT_ERROR = -1,                                ///< Error during initialization
-    INIT_DEVICE_NOT_SUPPORTED = -2,                 ///< The device is not supported
-    INIT_NO_CAMERA_ACCESS = -3,                     ///< Cannot access the camera
-    INIT_LICENSE_ERROR_MISSING_KEY = -4,            ///< License key is missing
-    INIT_LICENSE_ERROR_INVALID_KEY = -5,            ///< Invalid license key passed to SDK
-    INIT_LICENSE_ERROR_NO_NETWORK_PERMANENT = -6,   ///< Unable to verify license key due to network (Permanent error)
-    INIT_LICENSE_ERROR_NO_NETWORK_TRANSIENT = -7,   ///< Unable to verify license key due to network (Transient error)
-    INIT_LICENSE_ERROR_CANCELED_KEY = -8,           ///< Provided key is no longer valid
-    INIT_LICENSE_ERROR_PRODUCT_TYPE_MISMATCH = -9,  ///< Provided key is not valid for this product
-    INIT_EXTERNAL_DEVICE_NOT_DETECTED = -10         ///< Dependent external device not detected/plugged in
+    /** Error during initialization. */
+    INIT_ERROR = -1,
+    /** The device is not supported. */
+    INIT_DEVICE_NOT_SUPPORTED = -2,
+    /** Cannot access the camera. */
+    INIT_NO_CAMERA_ACCESS = -3,
+    /** License key is missing. */
+    INIT_LICENSE_ERROR_MISSING_KEY = -4,
+    /** Invalid license key passed to SDK. */
+    INIT_LICENSE_ERROR_INVALID_KEY = -5,
+    /** Unable to verify license key due to network (Permanent error). */
+    INIT_LICENSE_ERROR_NO_NETWORK_PERMANENT = -6,
+    /** Unable to verify license key due to network (Transient error). */
+    INIT_LICENSE_ERROR_NO_NETWORK_TRANSIENT = -7,
+    /** Provided key is no longer valid. */
+    INIT_LICENSE_ERROR_CANCELED_KEY = -8,
+    /** Provided key is not valid for this product. */
+    INIT_LICENSE_ERROR_PRODUCT_TYPE_MISMATCH = -9,
+    /** Dependent external device not detected/plugged in. */
+    INIT_EXTERNAL_DEVICE_NOT_DETECTED = -10
 };
 
-
+/**
+ * The different kinds of errors that vuforia can raise.
+ */
 export enum VuforiaErrorType {
     InitError = "InitError" as any,
     LoadDataSetError = "LoadDataSetError" as any,
@@ -34,6 +54,11 @@ export enum VuforiaErrorType {
     DeactivateDataSetError = "DeactivateDataSetError" as any,
 }
 
+/**
+ * An abstract class representing the functionality needed to implement
+ * Vuforia's functionality for a `VuforiaService`. This is a null
+ * implementation that simply does nothing in response to all requests.
+ */
 export class VuforiaServiceDelegate {
     isSupported() { return false }
     init(options: VuforiaInitOptions): any | PromiseLike<void> { }
@@ -54,36 +79,115 @@ export class VuforiaServiceDelegate {
     dataSetLoadEvent = new Event<VuforiaDataSetLoadMessage>();
 }
 
+/**
+ * The set of options accepted by Vuforia for initialization.
+ */
 export interface VuforiaInitOptions {
     licenseKey?: string,
     encryptedLicenseData?: string
 }
 
+/**
+ * Options for changing vuforia's background.
+ */
 export interface VuforiaVideoBackgroundConfig {
+    /** Whether the video is currently being captured and displayed. */
     enabled: boolean;
+    /** The offset of the video in pixels in the horizontal direction. */
     positionX: number;
+    /** The offset of the video in pixels in the vertical direction. */
     positionY: number;
+    /** The width of the video in pixels. */
     sizeX: number;
+    /** The height of the video in pixels. */
     sizeY: number;
 }
 
+/**
+ * Informtion on how video is being displayed by vuforia.
+ */
 export interface VuforiaVideoMode {
+    /** The width of the video in pixels. */
     width: number,
+    /** The height of the video in pixels. */
     height: number,
+    /** The number of frames being displayed per second in the video at this moment. */
     framerate: number
 }
 
+/**
+ * An error originating from vuforia.
+ */
 export interface VuforiaErrorMessage {
+    /** The type of error that was raised. */
     type: VuforiaErrorType,
+    /** A message giving more detail on the error. */
     message: string,
+    /** Any additional data related to the error. */
     data?: any
 }
 
+/**
+ * The response from vuforia once a data set has been loaded.
+ */
 export interface VuforiaDataSetLoadMessage {
+    /** The url from which the data set was loaded. */
     url: string,
+    /** The data set loaded. */
     trackables: VuforiaTrackables
 }
 
+/**
+ * Hooks up a context and its session with vuforia by allowing the context's
+ * sessions to make a number of requests:
+ *
+ *  * `ar.vuforia.isSupported` - Checks if vuforia is supported.
+ *    * Returns: True if vuforia is supported on the device argon is running
+ *      on and false if not.
+ *
+ *  * `ar.vuforia.init` - Initializes vuforia if it hasn't already been.
+ *    * Parameters:
+ *      * options: VuforiaInitOptions - The options to pass to vuforia as it is
+ *        being initialized.
+ *
+ *  * `ar.vuforia.deinit` - Deinitializes vuforia if it was previously
+ *    initialized by the same session.
+ *
+ *  * `ar.vuforia.startCamera` - Tells vuforia to startup the camera.
+ *
+ *  * `ar.vuforia.stopCamera` - Tells vuforia to shutdown the camera.
+ *
+ *  * `ar.vuforia.startObjectTracker` - Tells vuforia to startip its object
+ *    tracking system.
+ *
+ *  * `ar.vuforia.stopObjectTracker` - Tells vuforia to shutdown its object
+ *    tracking system.
+ *
+ *  * `ar.vuforia.hintMaxSimultaneousImageTargets` - Gives vuforia a hint on
+ *    the maximum number of targets it will be tracking.
+ *    * Parameters:
+ *      * options: {max: number} - The maximum number of targets vuforia will
+ *        be tracking for the session.
+ *
+ *  * `ar.vuforia.loadDataSet` - Asks vuforia to load a data set from the given
+ *    url.
+ *    * Parameters:
+ *      * options: {url: string} - The url pf the data set to load.
+ *
+ *  * `ar.vuforia.unloadDataSet` - Asks vuforia to unload a previously loaded
+ *    data set. Also deactivates the data set if it was activated.
+ *    * Parameters:
+ *      * options: {url: string} - The url of the data set to unload.
+ *
+ *  * `ar.vuforia.activateDataSet` - Asks vuforia to activate a data set. Also
+ *    loads the data set if it was not already loaded.
+ *    * Parameters:
+ *      * options: {url: string} - The url of the data set to activate.
+ *
+ *  * `ar.vuforia.deactivateDataSet` - Asks vuforia to deactivate a data set.
+ *    * Parameters:
+ *      * options: {url: string} - The url of the data set to deactivate.
+ */
 @inject(Context, RealityService, VuforiaServiceDelegate)
 export class VuforiaService {
 
@@ -357,6 +461,9 @@ export class VuforiaService {
 
 }
 
+/**
+ * A map from names of trackable data sets to their ids, names, and sizes TODO
+ */
 export interface VuforiaTrackables {
     [name: string]: {
         id: string,
@@ -365,6 +472,9 @@ export interface VuforiaTrackables {
     }
 }
 
+/**
+ * A vuforia data set. TODO
+ */
 export class VuforiaDataSet {
     constructor(public url: string, parentSession: Session) { this._parentSession = parentSession }
     private _parentSession: Session;
