b3d7b0997805f265bd901fc329cf4074f980fae6
Gheric Speiginer <gheric.speiginer@gmail.com>
2017-04-21 15:26:57 -0400

    perf(event): Reference the underlying functions directly

--- src/utils/event.ts
+++ src/utils/event.ts
@@ -24,9 +24,8 @@ export class Event<T> {
       * @param The function to be executed when the event is raised.
       * @return A convenience function which removes this event listener when called
       */
-    addEventListener(listener: (data: T) => void): RemoveCallback {
-        return this._event.addEventListener(listener);
-    }
+    addEventListener: (listener: (data: T) => void) => RemoveCallback =
+        this._event.addEventListener.bind(this._event);
 
     /**
      * Remove an event listener.
@@ -34,16 +33,14 @@ export class Event<T> {
      * @return True if the listener was removed; 
      * false if the listener and scope are not registered with the event.
      */
-    removeEventListener(listener: (data: T) => void): boolean {
-        return this._event.removeEventListener(listener);
-    }
+    removeEventListener: (listener: (data: T) => void) => boolean = 
+        this._event.removeEventListener.bind(this._event);
 
     /**
      * Raises the event by calling each registered listener with all supplied arguments.
      * @param This method takes any number of parameters and passes them through to the listener functions.
      */
-    raiseEvent(data: T): void {
-        this._event.raiseEvent(data);
-    }
+    raiseEvent: (data: T) => void = 
+        this._event.raiseEvent.bind(this._event);
 
 }
