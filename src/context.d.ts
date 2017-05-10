889cd6ad35d9dab712ff16eb04724949a7ffe15a
Taeheon Kim <bionictk@gmail.com>
2017-05-09 19:19:44 -0700

    Add permission support to argonjs

--- src/context.d.ts
+++ src/context.d.ts
@@ -3,6 +3,7 @@ import { Entity, EntityCollection, Cartographic, Cartesian3, Quaternion, Transfo
 import { SerializedEntityState, SerializedEntityStateMap, ContextFrameState, GeolocationOptions } from './common';
 import { SessionService, SessionPort } from './session';
 import { Event } from './utils';
+import { PermissionRequest } from './permissions';
 /**
  * Represents the pose of an entity relative to a particular reference frame.
  *
@@ -307,6 +308,7 @@ export declare class ContextServiceProvider {
     private _cacheTime;
     private _entityPoseCache;
     private _getSerializedEntityState;
+    requestPermission: (request: PermissionRequest) => Promise<boolean>;
     constructor(sessionService: SessionService, contextService: ContextService);
     fillEntityStateMapForSession(session: SessionPort, time: JulianDate, entities: SerializedEntityStateMap): void;
     private _publishUpdates();
