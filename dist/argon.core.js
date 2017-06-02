4fe917d6f6611bf95ee60206f4753b34355ff4f4
Gheric Speiginer <gheric.speiginer@gmail.com>
2017-06-01 20:18:06 -0400

    chore(build): Update build

--- dist/argon.core.js
+++ dist/argon.core.js
@@ -20922,7 +20922,7 @@ $__System.register('1', ['2', '3', '3b', '4', '9', '10', 'a', '1f', '32', '41', 
                 requestVertexNormals: true
             }));
 
-            _export('version', version = "1.2.2-1");
+            _export('version', version = "1.2.2-2");
 
             __extends = undefined && undefined.__extends || function (d, b) {
                 for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
@@ -25019,7 +25019,7 @@ $__System.register('1', ['2', '3', '3b', '4', '9', '10', 'a', '1f', '32', '41', 
                         });
                         realityControlSession.open(messageChannel.port2, _this.sessionService.configuration);
                     };
-                    var i = 0;
+                    // let i = 0;
                     this.contextService.updateEvent.addEventListener(function () {
                         var frameState = _this.contextService.serializedFrameState;
                         if (sessionService.isRealityViewer && sessionService.manager.isConnected) {
@@ -25030,7 +25030,8 @@ $__System.register('1', ['2', '3', '3b', '4', '9', '10', 'a', '1f', '32', '41', 
                                 eye.viewport = Viewport.clone(frameState.subviews[0].viewport, eye.viewport);
                                 delete frameState.entities['ar.user'];
                                 // throttle for 30fps
-                                i++ % 2 === 0 && sessionService.manager.send('ar.reality.frameState', frameState);
+                                // i++ % 2 === 0 && 
+                                sessionService.manager.send('ar.reality.frameState', frameState);
                                 frameState.entities['ar.user'] = eye.pose;
                             } else {
                                 sessionService.manager.send('ar.reality.frameState', frameState);
--- dist/argon.js
+++ dist/argon.js
@@ -26502,7 +26502,7 @@ $__System.register('1', ['2', '3', '3b', '4', '9', '10', 'a', '1f', '32', '41', 
                 requestVertexNormals: true
             }));
 
-            _export('version', version = "1.2.2-1");
+            _export('version', version = "1.2.2-2");
 
             __extends = undefined && undefined.__extends || function (d, b) {
                 for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
@@ -30599,7 +30599,7 @@ $__System.register('1', ['2', '3', '3b', '4', '9', '10', 'a', '1f', '32', '41', 
                         });
                         realityControlSession.open(messageChannel.port2, _this.sessionService.configuration);
                     };
-                    var i = 0;
+                    // let i = 0;
                     this.contextService.updateEvent.addEventListener(function () {
                         var frameState = _this.contextService.serializedFrameState;
                         if (sessionService.isRealityViewer && sessionService.manager.isConnected) {
@@ -30610,7 +30610,8 @@ $__System.register('1', ['2', '3', '3b', '4', '9', '10', 'a', '1f', '32', '41', 
                                 eye.viewport = Viewport.clone(frameState.subviews[0].viewport, eye.viewport);
                                 delete frameState.entities['ar.user'];
                                 // throttle for 30fps
-                                i++ % 2 === 0 && sessionService.manager.send('ar.reality.frameState', frameState);
+                                // i++ % 2 === 0 && 
+                                sessionService.manager.send('ar.reality.frameState', frameState);
                                 frameState.entities['ar.user'] = eye.pose;
                             } else {
                                 sessionService.manager.send('ar.reality.frameState', frameState);
