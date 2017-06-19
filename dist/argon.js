5ef5e9460accea5ce7973f9dcfc67b9ffb1f28d4
Kimberly Siva <kspreen@gmail.com>
2017-06-19 08:36:30 -0400

    chore(build): Update build

--- dist/argon.js
+++ dist/argon.js
@@ -26522,7 +26522,7 @@ $__System.register('1', ['2', '3', '3b', '4', '9', '10', 'a', '1f', '32', '41', 
                 requestVertexNormals: true
             }));
 
-            _export('version', version = "1.3.6");
+            _export('version', version = "1.3.7-0");
 
             __extends = undefined && undefined.__extends || function (d, b) {
                 for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
