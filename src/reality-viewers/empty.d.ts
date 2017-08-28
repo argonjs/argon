b96ef9adaa0ceb99741c76553561afd5c81ae961
Gheric Speiginer <gheric.speiginer@gmail.com>
2017-08-28 18:36:40 -0400

    chore(release): 1.4.0-25

--- src/reality-viewers/empty.d.ts
+++ src/reality-viewers/empty.d.ts
@@ -1,17 +1,19 @@
 import { Container } from 'aurelia-dependency-injection';
 import { SessionService } from '../session';
+import { DeviceService } from '../device';
 import { ViewService } from '../view';
 import { RealityViewer } from './base';
 export declare class EmptyRealityViewer extends RealityViewer {
     private sessionService;
     private viewService;
     private container;
+    private deviceService;
     uri: string;
     type: string;
     userTracking: 'none' | '3DOF' | '6DOF';
     private _aggregator;
     private _moveFlags;
-    constructor(sessionService: SessionService, viewService: ViewService, container: Container, uri: string);
+    constructor(sessionService: SessionService, viewService: ViewService, container: Container, deviceService: DeviceService, uri: string);
     private _scratchMatrix3;
     private _scratchMatrix4;
     load(): void;
